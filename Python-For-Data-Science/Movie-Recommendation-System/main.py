f99ef921ab7adb70636a118f295e650783f2661e
shubhamnarkhede <shubhamvnarkhede@gmail.com>
2020-06-30 01:48:54 +0530

    File Updated

--- Python-For-Data-Science/Movie-Recommendation-System/main.py
+++ Python-For-Data-Science/Movie-Recommendation-System/main.py
@@ -7,7 +7,7 @@ from lightfm import LightFM
 data = fetch_movielens(min_rating=4.0)
 
 #print training and testing data
-print(repe(data['train']))
+print(repr(data['train']))
 print(repr(data['test']))
 
 #create model
@@ -26,3 +26,23 @@ def sample_recommendation(model, data, user_ids):
 
 		#movies they already like
 		know_positives = data['item_labels'][data['train'].tocsr()[user_id].indices]
+
+		#movies our model predicts they will like
+		scores = model.predict(user_id, np.arange(n_items))
+		#rank them in order of most liked to least
+		top_items = data['item_labels'][np.argsort(-scores)]
+
+		#print out the results
+		print("User %s" % user_id)
+		print("     Known positives:")
+
+		for x in know_positives[:3]:
+			print("           %s" % x)
+
+		print("       Recommended:")
+
+		for x in top_items[:3]:
+			print("           %s" % x)
+
+sample_recommendation(model, data, [3, 25, 450])
+
